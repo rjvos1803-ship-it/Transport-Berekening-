ebeac9c747b31922c3ef1ba6bc106882c6aa5a2a
rjvos1803-ship-it <rjvos1803@gmail.com>
2025-10-23 06:42:42 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ✅ Vite-configuratie voor React + Tailwind
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
+  },
+  server: {
+    port: 5173
   }
 })
